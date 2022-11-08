4f5332736ca33c3ecc61f18c270a17dd68220d80
sandeep-subramani <103183369+sandeep-subramani@users.noreply.github.com>
2022-11-08 15:43:58 +0530

    Update script.sh

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -16,25 +16,4 @@ npm install -g expo-cli
 # build generation
 
 expo build:web
-
-# clone web-build repo and remove all
-
-cd ..
-git clone git@github.com:sandeep-subramani/web-build.git -b main
-cd web-build
-rm -rf * | echo "y"
-cd ..
-
-# copy new build and paste in web-build cloned folder
-
-cd GitHubAutomation/web-build
-cp -R ./* ../../web-build/
-
-# push changes to web-build repo
-
-cd ../../web-build
-git status
-git add .
-git commit -m "GitHubAutomation Updated"
-git push
-
+echo "web building complete..."
