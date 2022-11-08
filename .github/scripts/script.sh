232375cc06df36e0e37d9ffd429d6d99d7754140
sandeep-subramani <103183369+sandeep-subramani@users.noreply.github.com>
2022-11-08 11:08:21 +0530

    Update script.sh

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-cd ./assets
+# installiing / updgrading npm and node
+
+npm install -g npm
+npm install -g n
+
+# installing npm packages
+
+npm install
+
+# installing expo-cli
+
+npm install -g expo-cli
+
+# build generation and check
+
+expo build:web
+cd web-build
 ls
-cd ..
-node -v && npm -v
